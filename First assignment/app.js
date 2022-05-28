069f170c6db3ed38dfccf2c25b020a421e0fd87c
Wagner Almeida <63356947+Aladiah96@users.noreply.github.com>
2022-05-28 09:48:31 -0300

    First assignment
    I've just finished my first assignment to tt hhe course and started studying th NPM Scripts module.

--- First assignment/app.js
+++ First assignment/app.js
@@ -9,8 +9,8 @@ http.createServer((req,res) => {
         res.write('<body>');
         res.write('<h1>Hello!!!</h1>');
         res.write('<form action="/create-user" method="POST">');
-        res.write('<input type="text" placeholder="Username"></input>');
-        res.write('<button name="Send" value="send">Send</button>');
+        res.write('<input type="text" name="newUser" placeholder="Username">');
+        res.write('<button type="submit">Send</button>');
         res.write('</form>');
         res.write('</body>');
         res.write('</html>');
@@ -24,24 +24,14 @@ http.createServer((req,res) => {
         res.write('</html>');
     } else if (url === '/create-user' && method === 'POST') {
         const body = [];
-        req.on('data', (name) => {
+        req.on('data', name => {
             body.push(name);
-        })
-        const parsedBody = Buffer.concat(body).toString();
-        const user = parsedBody.split('=')[1];
-        console.log(user);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const user = parsedBody.split('=')[1];
+            console.log('user' + user);
+        });
     }
     
 }).listen(3000);
-
-// Spin up a node.js-driven server (on port 3000)
-
-// Handle two routes: "/" and "/user"
-// Return some greeting text on "/"
-// Return a list of dummy users(e.g<ul><li>User 1</li></ul>)
-
-//Add a form with a "username" <input> to the "/" page and submit a
-// POST request to "/create-user" upon a button click
-
-// Add the "/create-user" route and parse the incoming data
-// and simply log it to the console.
