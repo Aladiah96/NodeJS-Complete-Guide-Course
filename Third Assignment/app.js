729bcd9e4b6b2dfebdcac75003cc6b169ce64094
Aladiah <wagner.aladiah@gmail.com>
2022-06-11 13:06:00 -0300

    Revert "Just finished the third assignment of the course."
    This reverts commit e3e042499430bf459b073fbb552fd9fa5814405e.

--- Third Assignment/app.js
+++ Third Assignment/app.js
@@ -1,17 +1,9 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/users', (req,res,next) => {
-    console.log('Second route');
-    res.sendFile(path.join(__dirname, 'views', 'users.html'))
-});
-
-app.use('/', (req,res,next) => {
-    console.log('First route');
-    res.sendFile(path.join(__dirname, 'views', 'home.html'))
+app.get('/', (req,res,next) => {
+  res.sendFile(path.join(__dirname, 'views/main.html'));
 });
 
 app.listen(3000);
