92bc127e008b501ae73ac2f83e7fcded11042441
Aladiah <wagner.aladiah@gmail.com>
2022-09-07 15:54:11 -0300

    Quick study session on Dynamic Routes & Advanced Models

---
+++ 00-starting-setup/prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowParens: 'avoid',
+  singleQUote: true,
+  bracketSpacing: true,
+  endOfline: 'lf',
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'none'
+}
+
+module.exports = options
